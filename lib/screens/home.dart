9f4bdf3b6ed7d3c8b728a09b9b6578e87c246819
Dee <dee.markanthony@gmail.com>
2024-09-27 00:06:25 +0800

    update background of home page

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,6 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:s2s_after_sales/blocs/auth.dart';
+import 'package:s2s_after_sales/components/background.dart';
 
 import '../utils/navigator.dart';
 
@@ -13,66 +12,41 @@ class HomePage extends StatelessWidget {
     ThemeData _theme = Theme.of(context);
     AuthBloc _auth = AuthBloc.instance(context)!;
 
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Container(
-        color: Colors.white,
-        child: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("assets/images/login-bg.png"),
-              fit: BoxFit.cover,
-            ),
-          ),
-          child: BackdropFilter(
-            filter: ImageFilter.blur(sigmaX: 25, sigmaY: 25),
-            child: Container(
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [Colors.transparent, Colors.black12],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                ),
-              ),
-              child: Center(
-                child: Padding(
+    return Background(
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: 480, maxHeight: 520),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const SizedBox(height: 60),
+                const Spacer(),
+                Container(
                   padding: const EdgeInsets.all(16),
-                  child: ConstrainedBox(
-                    constraints:
-                        const BoxConstraints(maxWidth: 480, maxHeight: 520),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        const SizedBox(height: 60),
-                        const Spacer(),
-                        Container(
-                          padding: const EdgeInsets.all(16),
-                          decoration: BoxDecoration(
-                            color: _theme.primaryColorLight.withOpacity(0.25),
-                            borderRadius: BorderRadius.circular(16),
-                          ),
-                          child: Text(
-                            _auth.currentAccount.toString(),
-                            style: _theme.primaryTextTheme.titleMedium,
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 32,
-                        ),
-                        ElevatedButton(
-                          onPressed: () {
-                            _auth.logout();
-                            Navigator.of(context).popUntilLogin();
-                          },
-                          child: const Text("Log out"),
-                        ),
-                        const Spacer(),
-                        const SizedBox(height: 60),
-                      ],
-                    ),
+                  decoration: BoxDecoration(
+                    color: _theme.primaryColorLight.withOpacity(0.25),
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Text(
+                    _auth.currentAccount.toString(),
+                    style: _theme.primaryTextTheme.titleMedium,
                   ),
                 ),
-              ),
+                const SizedBox(
+                  height: 32,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    _auth.logout();
+                    Navigator.of(context).popUntilLogin();
+                  },
+                  child: const Text("Log out"),
+                ),
+                const Spacer(),
+                const SizedBox(height: 60),
+              ],
             ),
           ),
         ),
