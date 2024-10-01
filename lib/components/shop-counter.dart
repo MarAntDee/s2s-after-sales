40574cb25a8704ad47388581dde22de495291023
Dee <dee.markanthony@gmail.com>
2024-10-01 11:10:01 +0800

    fix layout bug of shop counter

--- lib/components/shop-counter.dart
+++ lib/components/shop-counter.dart
@@ -36,130 +36,137 @@ class _ShopCounterState extends State<ShopCounter> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        const SizedBox(height: 32),
-        Text(
-          "Make sure the details are correct",
-          style: theme.textTheme.titleMedium!
-              .copyWith(fontWeight: FontWeight.w700),
-          textAlign: TextAlign.center,
-        ),
-        const SizedBox(height: 32),
-        Padding(
-          padding: const EdgeInsets.only(left: 16),
-          child: Text(
-            "Purchase Summary",
-            style: theme.textTheme.bodyLarge,
-          ),
-        ),
-        const SizedBox(height: 24),
-        ProductCard(shopKeeper.selectedProduct!, selected: true),
-        const SizedBox(height: 16),
-        Text(
-          "\t\t\t\tSelect Payment Method",
-          style: Theme.of(context).textTheme.bodySmall,
-        ),
-        const Divider(indent: 8, endIndent: 8),
-        Expanded(
-          child: FutureBuilder<List<PaymentMethod>>(
-            future: shopKeeper.getPaymentMethodList,
-            builder: (context, paymentMethods) {
-              if (!paymentMethods.hasData) {
-                return const Center(
-                  child: SizedBox.square(
-                    dimension: 60,
-                    child: CircularProgressIndicator(),
-                  ),
-                );
-              }
-              return ListView(
-                physics: const ClampingScrollPhysics(),
-                children: paymentMethods.data!
-                    .map<Widget>(
-                      (paymentMethod) => RadioListTile<PaymentMethod>(
-                        title: Row(
-                          children: [
-                            Container(
-                              width: 60,
-                              height: 40,
-                              decoration: BoxDecoration(
-                                border:
-                                    Border.all(color: Colors.black54, width: 1),
-                                borderRadius: BorderRadius.circular(4),
-                              ),
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(4),
-                                child: Image(
-                                  image: paymentMethod.image,
-                                  fit: BoxFit.fitWidth,
-                                ),
-                              ),
-                            ),
-                            const SizedBox(width: 8),
-                            Text(paymentMethod.name),
-                          ],
-                        ),
-                        onChanged: (paymentMethod) => setState(() =>
-                            shopKeeper.selectedPaymentMethod = paymentMethod),
-                        value: paymentMethod,
-                        groupValue: shopKeeper.selectedPaymentMethod,
+    return ConstrainedBox(
+      constraints: const BoxConstraints(minHeight: 800),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            const SizedBox(height: 32),
+            Text(
+              "Make sure the details are correct",
+              style: theme.textTheme.titleMedium!
+                  .copyWith(fontWeight: FontWeight.w700),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 32),
+            Padding(
+              padding: const EdgeInsets.only(left: 16),
+              child: Text(
+                "Purchase Summary",
+                style: theme.textTheme.bodyLarge,
+              ),
+            ),
+            const SizedBox(height: 24),
+            ProductCard(shopKeeper.selectedProduct!, selected: true),
+            const SizedBox(height: 16),
+            Text(
+              "\t\t\t\tSelect Payment Method",
+              style: Theme.of(context).textTheme.bodySmall,
+            ),
+            const Divider(indent: 8, endIndent: 8),
+            SizedBox(
+              height: 360,
+              child: FutureBuilder<List<PaymentMethod>>(
+                future: shopKeeper.getPaymentMethodList,
+                builder: (context, paymentMethods) {
+                  if (!paymentMethods.hasData) {
+                    return const Center(
+                      child: SizedBox.square(
+                        dimension: 60,
+                        child: CircularProgressIndicator(),
                       ),
-                    )
-                    .toList(),
-              );
-            },
-          ),
-        ),
-        const Divider(indent: 8, endIndent: 8),
-        const SizedBox(height: 4),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
-          child: Row(
-            children: [
-              const Text("Subtotal:"),
-              const Spacer(),
-              Text(priceText),
-            ],
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
-          child: Row(
-            children: [
-              const Text("Convenience fee:"),
-              const Spacer(),
-              Text(convenienceFeeText),
-            ],
-          ),
-        ),
-        const SizedBox(height: 16),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8),
-          child: Row(
-            children: [
-              Text(
-                "Total:",
-                style: theme.textTheme.headlineSmall,
+                    );
+                  }
+                  return ListView(
+                    physics: const ClampingScrollPhysics(),
+                    children: paymentMethods.data!
+                        .map<Widget>(
+                          (paymentMethod) => RadioListTile<PaymentMethod>(
+                            title: Row(
+                              children: [
+                                Container(
+                                  width: 60,
+                                  height: 40,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                        color: Colors.black54, width: 1),
+                                    borderRadius: BorderRadius.circular(4),
+                                  ),
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(4),
+                                    child: Image(
+                                      image: paymentMethod.image,
+                                      fit: BoxFit.fitWidth,
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(width: 8),
+                                Text(paymentMethod.name),
+                              ],
+                            ),
+                            onChanged: (paymentMethod) => setState(() =>
+                                shopKeeper.selectedPaymentMethod =
+                                    paymentMethod),
+                            value: paymentMethod,
+                            groupValue: shopKeeper.selectedPaymentMethod,
+                          ),
+                        )
+                        .toList(),
+                  );
+                },
               ),
-              const Spacer(),
-              Text(
-                totalPriceText,
-                style: theme.textTheme.headlineMedium!
-                    .copyWith(color: theme.colorScheme.secondary),
+            ),
+            const Divider(indent: 8, endIndent: 8),
+            const SizedBox(height: 4),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+              child: Row(
+                children: [
+                  const Text("Subtotal:"),
+                  const Spacer(),
+                  Text(priceText),
+                ],
               ),
-            ],
-          ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+              child: Row(
+                children: [
+                  const Text("Convenience fee:"),
+                  const Spacer(),
+                  Text(convenienceFeeText),
+                ],
+              ),
+            ),
+            const SizedBox(height: 16),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: Row(
+                children: [
+                  Text(
+                    "Total:",
+                    style: theme.textTheme.headlineSmall,
+                  ),
+                  const Spacer(),
+                  Text(
+                    totalPriceText,
+                    style: theme.textTheme.headlineMedium!
+                        .copyWith(color: theme.colorScheme.secondary),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              child: ElevatedButton(
+                onPressed: ProdApi().getPaymentMethods,
+                child: const Text("Buy Load"),
+              ),
+            ),
+          ],
         ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-          child: ElevatedButton(
-            onPressed: ProdApi().getPaymentMethods,
-            child: const Text("Buy Load"),
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
