eeeda8f78b00f843554e6ccb6cfea2635f9936c2
Dee <dee@Dees-MacBook-Pro.local>
2024-10-22 15:57:44 +0800

    add user card in payment journal

--- lib/screens/payment-journal.dart
+++ lib/screens/payment-journal.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:surf2sawa/blocs/auth.dart';
 import 'package:surf2sawa/components/background.dart';
+import 'package:surf2sawa/theme/app.dart';
 
 import '../components/empty.dart';
 import '../components/error.dart';
@@ -31,47 +32,108 @@ class PaymentJournal extends StatelessWidget {
             title: const Text("Payment History"),
           ),
           backgroundColor: Colors.transparent,
-          body: Column(
+          body: Stack(
             children: [
-              const SizedBox(height: 80),
-              Expanded(
-                child: Container(
-                  color: theme.scaffoldBackgroundColor,
-                  padding: const EdgeInsets.only(top: 80),
-                  child: Center(
-                    child: ConstrainedBox(
-                      constraints: const BoxConstraints(maxWidth: 600),
-                      child: FutureBuilder<List<Transaction>>(
-                        future: ProdApi().getPaymentHistory(),
-                        builder: (context, transactions) {
-                          if (transactions.hasError) {
-                            if (transactions.error?.toString() ==
-                                "You are not allowed in this resource") {
-                              auth.logout(autologout: true);
-                            } else {
-                              return ErrorDisplay.list(transactions.error);
-                            }
-                          }
-                          if (!transactions.hasData) {
-                            return const Center(
-                              child: SizedBox.square(
-                                dimension: 60,
-                                child: CircularProgressIndicator(),
+              Column(
+                children: [
+                  const SizedBox(height: 80),
+                  Expanded(
+                    child: Container(
+                      color: theme.scaffoldBackgroundColor,
+                      padding: const EdgeInsets.only(top: 80),
+                      child: Center(
+                        child: ConstrainedBox(
+                          constraints: const BoxConstraints(maxWidth: 600),
+                          child: FutureBuilder<List<Transaction>>(
+                            future: ProdApi().getPaymentHistory(),
+                            builder: (context, transactions) {
+                              if (transactions.hasError) {
+                                if (transactions.error?.toString() ==
+                                    "You are not allowed in this resource") {
+                                  auth.logout(autologout: true);
+                                } else {
+                                  return ErrorDisplay.list(transactions.error);
+                                }
+                              }
+                              if (!transactions.hasData) {
+                                return const Center(
+                                  child: SizedBox.square(
+                                    dimension: 60,
+                                    child: CircularProgressIndicator(),
+                                  ),
+                                );
+                              }
+                              if (transactions.data!.isEmpty) {
+                                return EmptyDisplay.list(
+                                  "History is empty",
+                                  Icons.book_rounded,
+                                );
+                              }
+                              return ListView(
+                                children: transactions.data!
+                                    .map((tx) => TransactionTile(tx))
+                                    .toList(),
+                              );
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Positioned(
+                top: 40,
+                left: 24,
+                right: 24,
+                height: 80,
+                child: Center(
+                  child: ConstrainedBox(
+                    constraints: const BoxConstraints(maxWidth: 480),
+                    child: Container(
+                      height: 80,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      padding: const EdgeInsets.all(16),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisSize: MainAxisSize.min,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Text(
+                                  auth.currentAccount!.currentProduct!.toString(),
+                                  style: theme.textTheme.titleLarge!.copyWith(
+                                    color: theme.colorScheme.darkGrayText,
+                                    fontWeight: FontWeight.w700,
+                                  ).apply(fontSizeDelta: -4),
+                                ),
+                                Text(
+                                  auth.currentAccount!.expirationText!,
+                                  style: theme.textTheme.labelSmall!.copyWith(
+                                    color: (auth.currentAccount!.isExpired ?? true)
+                                        ? theme.colorScheme.error
+                                        : theme.colorScheme.lightGrayText,
+                                ),
+                                )
+                              ],
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: auth.pushToShop,
+                            child: const SizedBox(
+                              width: 80,
+                              child: Center(
+                                child: Text("Buy Load"),
                               ),
-                            );
-                          }
-                          if (transactions.data!.isEmpty) {
-                            return EmptyDisplay.list(
-                              "History is empty",
-                              Icons.book_rounded,
-                            );
-                          }
-                          return ListView(
-                            children: transactions.data!
-                                .map((tx) => TransactionTile(tx))
-                                .toList(),
-                          );
-                        },
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ),
