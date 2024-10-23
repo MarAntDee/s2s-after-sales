f475b565196f1592f8a92e272d7905d6928e7d38
Dee <dee.markanthony@gmail.com>
2024-10-24 00:33:48 +0800

    update icons

---
+++ lib/theme/icons.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class IconLibrary {
+  IconLibrary._();
+
+  static const _kFontFam = 'Surf2Sawa';
+  static const String? _kFontPkg = null;
+
+  static const IconData payment_entry =
+      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData dashboard_home =
+      IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData check_sharp =
+      IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData receipt =
+      IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData wifi_tower =
+      IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData notification_bell =
+      IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData partial_menu =
+      IconData(0xe806, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
